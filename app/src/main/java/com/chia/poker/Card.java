6bdc84b35735653f405e4a8da0d3eb828155ed09
cac0709 <cac91053@gmail.com>
2018-12-10 20:05:24 +0800

    first commit

---
+++ app/src/main/java/com/chia/poker/Card.java
@@ -0,0 +1,14 @@
+package com.chia.poker;
+
+public class Card {
+    char [] suits =  {'\u2663', '\u2666', '\u2665', '\u2660'};
+    int value ;
+    public Card(int value) {
+        this.value = value;
+    }
+
+    public String get() {
+        return value%13+1 + "" + suits[value/13];
+    }
+}
+
